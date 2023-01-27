e6e667f81bb7166994aba046cfb4ce5ecdbe66f4
KianaMsqt <masqati.kiana@gmail.com>
2023-01-27 18:45:03 +0000

    Add basic api request

--- statics/javascript/scripts.js
+++ statics/javascript/scripts.js
@@ -1,2 +1,23 @@
-// API Key 
-// 52a07063d5727308908d9b356e188a05
+$( '#search-button' ).on( 'click', function( e ) {
+    e.preventDefault();
+    
+    var inputQuery = $( '#search-input' ).val().trim(),
+        apiKey = "52a07063d5727308908d9b356e188a05",
+        lat,
+        long,
+        queryURL;
+
+        // TODO: get lat and long, using inputQuery
+
+        queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&appid=" + apiKey;
+
+    $.ajax( {
+        url: queryURL,
+        method: "GET"
+    } ).then( function( response ) {
+        console.log( response )
+    } );
+
+} )
+
+// TODO: Declare a function to delete recent results and call it before requesting for search
